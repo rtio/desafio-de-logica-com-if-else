709493c4fd6700446712fb0a8fdd6bb826991976
AlexsanderDiego <alexdiegoat@gmail.com>
2023-06-01 23:07:20 -0300

    Finalizando a questão 01

--- verificarIdade.js
+++ verificarIdade.js
@@ -1,4 +1,4 @@
-// **Verificar a idade**: Crie um programa que peça ao usuário para inserir a sua idade e informe se o usuário é menor de idade (menos de 18 anos) ou maior de idade (18 anos ou mais).
+// 1. **Verificar a idade**: Crie um programa que peça ao usuário para inserir a sua idade e informe se o usuário é menor de idade (menos de 18 anos) ou maior de idade (18 anos ou mais).
 
 const prompt = require('prompt-sync')()
 
@@ -10,4 +10,3 @@ if (idade >= 18) {
 } else {
     console.log(`Você ainda é menor de idade.`)
 }
-
