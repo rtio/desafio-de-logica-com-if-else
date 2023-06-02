8a834af87b352ba690f8b53ca361c9ef8c83e38a
AlexsanderDiego <alexdiegoat@gmail.com>
2023-06-01 23:11:15 -0300

    Adicionando questão 02

---
+++ calculadora.js
@@ -0,0 +1,1 @@
+// 2. **Calculadora básica**: Crie uma calculadora básica, onde o usuário fornece dois números e a operação que deseja realizar (adição, subtração, multiplicação, divisão). Use if e else para determinar a operação a ser realizada.
