e35f121bb1f4d784c1d36a94ed0d616e67335dba
Alexsander Diego <44487594+AlexsanderDiego@users.noreply.github.com>
2023-06-06 11:40:35 -0300

    Update classificarNumeros.js

--- classificarNumeros.js
+++ classificarNumeros.js
@@ -1,23 +1,21 @@
 // 4. **Classificar números**: Crie um programa que peça ao usuário para inserir três números diferentes e, em seguida, imprima os números em ordem crescente.
 
-const prompt = require('prompt-sync')()
+//const prompt = require('prompt-sync')()
 
 const num1 = prompt('Primeiro número? ')
 const num2 = prompt('Segundo número? ')
 const num3 = prompt('Terceiro número? ')
 
-if (num1 >= num2 && num1 >= num3) {
-    console.log(`O número ${num1} é o maior.`)
-} else if (num2 >= num1 && num2 >= num3) {
-    console.log(`O número ${num2} é o maior.`)
+if (num1 >= num2 >= num3) {
+    console.log(`Ordem crescente: ${num3}, ${num2}, ${num1}.`)
+} else if (num1 >= num3 >= num2) {
+    console.log(`Ordem crescente: ${num2}, ${num3}, ${num1}.`)
+} else if (num2 >= num1 >= num3) {
+    console.log(`Ordem crescente: ${num3}, ${num1}, ${num2}.`)
+} else if (num2 >= num3 >= num1) {
+    console.log(`Ordem crescente: ${num1}, ${num3}, ${num2}.`)
+} else if (num3 >= num1 >= num2) {
+    console.log(`Ordem crescente: ${num2}, ${num1}, ${num3}.`)
 } else {
-    console.log(`O número ${num3} é o maior.`)
-}
-
-if (num1 <= num2 && num1 <= num3) {
-    console.log(`O número ${num1} é o menor.`)
-} else if (num2 <= num1 && num2 <= num3) {
-    console.log(`O número ${num2} é o menor.`)
-} else {
-    console.log(`O número ${num3} é o menor.`)
+    console.log(`Ordem crescente: ${num1}, ${num2}, ${num3}.`)
 }
