2d07581981e6882c5def25b35a6d34e0bc003bd5
juliherculano <juliherculano@gmail.com>
2023-06-19 20:22:31 -0300

    adiciona novas resolucoes

--- calculadora.js
+++ calculadora.js
@@ -1,23 +1,43 @@
+// const prompt = require('prompt-sync')();
+
+// const numero1 = parseFloat(prompt('Digite o primeiro número: '));
+// const numero2 = parseFloat(prompt('Digite o segundo número: '));
+// const operacao = prompt('Digite a operação desejada: ');
+
+// function calcula() {
+//   if (operacao === '+') {
+//     return numero1 + numero2;
+//   } else if (operacao === '-') {
+//     return numero1 - numero2;
+//   } else if (operacao === '*') {
+//     return numero1 * numero2;
+//   } else if (operacao === '/') {
+//     return numero1 / numero2;
+//   } else {
+//     return 'Operação inválida!';
+//   };
+// };
+
+// const resultado = calcula();
+// console.log(`Resultado: ${numero1} ${operacao} ${numero2} = ${resultado}`);
+
+//// FUNÇÃO 2 ////
+
 const prompt = require('prompt-sync')();
 
-const numero1 = parseFloat(prompt('Digite o primeiro número: '));
-const numero2 = parseFloat(prompt('Digite o segundo número: '));
+const numero1 = Number(prompt('Digite o primeiro número: '));
+const numero2 = Number(prompt('Digite o segundo número: '));
 const operacao = prompt('Digite a operação desejada: ');
 
 function calcula() {
   if (operacao === '+') {
-    return numero1 + numero2;
-  } else if (operacao === '-') {
-    return numero1 - numero2;
-  } else if (operacao === '*') {
-    return numero1 * numero2;
-  } else if (operacao === '/') {
-    return numero1 / numero2;
-  } else {
-    return 'Operação inválida!';
-  };
+  const resultado = numero1 + numero2;
+  return `O resultado da soma é: ${numero1 + numero2}`;
+  } if (operacao === '-') {
+  const subtracao = numero1 - numero2;
+  return 'O resultado da subtração é: ' + subtracao;
+  }
 };
 
-const resultado = calcula();
-console.log(`Resultado: ${numero1} ${operacao} ${numero2} = ${resultado}`);
-
+const output = calcula();
+console.log(output);
--- idade.js
+++ idade.js
@@ -1,11 +1,34 @@
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 
-const idade = prompt('Digite sua idade: ');
+// const idade = prompt('Digite sua idade: ');
 
-const maiorIdade = 18;
+// const maiorIdade = 18;
 
-if (idade < maiorIdade) {
-    console.log('Você é menor de idade!');
-} else {
-    console.log('Você é maior de idade!');
+// if (idade < maiorIdade) {
+//     console.log('Você é menor de idade!');
+// } else {
+//     console.log('Você é maior de idade!');
+// };
+
+//// FEITO COM FUNÇÃO ////
+
+const prompt = require("prompt-sync")();
+
+const idadeInput = prompt("Digite sua idade: ");
+
+function verificarIdade(idade) {
+    if (isNaN(idade)) {
+        return 'A sua entrada não é um número!'
+    }
+    if (idade < 0.1) {
+        return 'Entrada inválida!';
+    } if (idade < 18) {
+        return 'Você é menor de idade!';
+    } if (idade < 64)  {
+    return 'Você é maior de idade!';
+    }
+    return 'Você é idoso!'
 };
+
+const output = verificarIdade(idadeInput);
+console.log(output);
